d118d3a97daf82dfec7c6e12d378948c65641691
garuhester <garuhester@email.com>
2018-09-18 18:52:57 +0800

    u

--- src/Level.js
+++ src/Level.js
@@ -1,5 +1,10 @@
 var Menu = function () {
 	this.preload = function () {
+		var menu = game.add. text(game.world.centerX, game.world.centerY, "GAME", {
+			fill: '#fff'
+		});
+		menu.anchor.set(0.5, 0.5);
+		
 		// 添加点击事件
 		game.input.onTap.add(function () {
 			game.state.start('Play');
